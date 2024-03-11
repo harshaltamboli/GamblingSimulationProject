69091843cce93f9f78050a73b49a64505f77a6ec
harshaltamboli <harshaltamboli429@gmail.com>
2024-03-12 03:43:49 +0530

    [ADDED]: harshal print the mesage welcome Gambling Simulation

---
+++ src/com/harshal/gamblgprblum/GambligSimulation.java
@@ -0,0 +1,10 @@
+package com.harshal.gamblgprblum;
+
+public class GambligSimulation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Gambling Simulation problem Developed by harshal Tamboli");
+		
+	}
+
+}
